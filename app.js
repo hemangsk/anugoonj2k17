d2a82d4701f5c50c3766ec65075b9d53d0b621e7
Hemang Kumar <hemangsk@gmail.com>
2017-02-02 00:14:40 +0530

    angular: implemented angular routing

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('anugoonj', [])
+var app = angular.module('anugoonj', ['ngRoute'])
 app.controller('EventController', function ($scope) {
 	 self = this
 	 self.click = function () {
@@ -6,3 +6,67 @@ app.controller('EventController', function ($scope) {
 	 	$('#modal1').modal('open');
 	 }
 })
+
+app.config(['$routeProvider',
+		function($routeProvider) {
+			$routeProvider.
+			when('/', {
+				redirectTo: '/home'
+			}).
+			when('/home', {
+				template: '<home></home>'
+			}).
+			when('/events', {
+				template: '<events></events>'
+			}).
+			when('/chat', {
+				template: '<chat></chat>'
+			}).
+			when('/sponsors', {
+				template: '<sponsors></sponsors>'
+			})
+		}]);
+
+app.controller('TabController', function ($location, $scope) {
+		$scope.tab = $location.path()
+		$scope.setTab = function (stab) {
+			$scope.tab = stab
+			console.log(stab);
+			$location.path(stab);
+			$scope.$evalAsync()
+			$(document).ready(function () {
+		$('.button-collapse').sideNav('hide');
+			})
+		}
+		$scope.isSet = function (stab) {
+			return $scope.tab == stab
+		}
+	})
+
+	app.directive('home',[ '$http', function ($http) {
+		return {
+			restrict: 'E',
+			templateUrl: '/partials/tabs/home.html'
+		}
+	}]);
+
+		app.directive('events',[ '$http', function ($http) {
+		return {
+			restrict: 'E',
+			templateUrl: '/partials/tabs/events.html'
+		}
+	}]);
+
+			app.directive('sponsors',[ '$http', function ($http) {
+		return {
+			restrict: 'E',
+			templateUrl: '/partials/tabs/sponsors.html'
+		}
+	}]);
+
+				app.directive('chat',[ '$http', function ($http) {
+		return {
+			restrict: 'E',
+			templateUrl: '/partials/tabs/chat.html'
+		}
+	}]);
